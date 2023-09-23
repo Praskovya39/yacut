c2acd8dbf6d99b849c0d453d7f89856f451e4f03
Екатерина Толмачева <kathy.tolmacheva@gmail.com>
2023-09-23 11:35:10 +0300

    for review

---
+++ yacut/api_views.py
@@ -0,0 +1,37 @@
+from http import HTTPStatus
+
+from flask import jsonify, request
+
+from . import app, db
+from .error_handlers import InvalidAPIUsage, check_inique_short_url
+from .models import URLMap
+from .utils import check_symbols, get_unique_short_url
+
+
+@app.route('/api/id/', methods=['POST'])
+def add_url():
+    data = request.get_json()
+    if not data:
+        raise InvalidAPIUsage('Отсутствует тело запроса', HTTPStatus.BAD_REQUEST)
+    if 'url' not in data:
+        raise InvalidAPIUsage('"url" является обязательным полем!')
+    if 'custom_id' not in data or data['custom_id'] is None:
+        data['custom_id'] = get_unique_short_url()
+    custom_id = data['custom_id']
+    if len(custom_id) > 16 or not check_symbols(custom_id):
+        raise InvalidAPIUsage('Указано недопустимое имя для короткой ссылки')
+    if check_inique_short_url(custom_id):
+        raise InvalidAPIUsage((f'Имя "{custom_id}" уже занято.'))
+    url = URLMap()
+    url.from_dict(data)
+    db.session.add(url)
+    db.session.commit()
+    return jsonify(url.to_dict()), HTTPStatus.CREATED
+
+
+@app.route('/api/id/<string:short_id>/', methods=['GET'])
+def get_original_url(short_id):
+    url = URLMap.query.filter_by(short=short_id).first()
+    if not url:
+        raise InvalidAPIUsage('Указанный id не найден', HTTPStatus.NOT_FOUND)
+    return jsonify({'url': url.original})
